ae81d320ecea93f2e4557d5c89d50d29d6284c37
Виктор Ганзенко <viktorganzenko@MacBook-Pro-Viktor.local>
2022-11-20 11:06:06 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galeryGard = galleryItems.map(createGalleryItem).join("");
-const galleryEL = document.querySelector(".gallery");
-galleryEL.insertAdjacentHTML("afterbegin", galeryGard);
-galleryEL.addEventListener("click", modalOpen);
-let modalWindow;
+const galleryRef = document.querySelector(".gallery");
+const galleryCardSet = galleryItems.map(createGalleryCard).join("");
 
-function createGalleryItem({ preview, original, description }) {
+galleryRef.insertAdjacentHTML("afterbegin", galleryCardSet);
+
+galleryRef.addEventListener("click", onOpenModal);
+
+let modal; // = basicLightbox.create
+
+function createGalleryCard({ preview, original, description }) {
   return `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img
@@ -20,33 +23,34 @@ function createGalleryItem({ preview, original, description }) {
   </div>`;
 }
 
-function modalOpen(element) {
-  element.preventDefault();
-  const galleryImg = element.target.classList.contains("gallery__image");
-  if (!galleryImg) {
+function onOpenModal(e) {
+  e.preventDefault();
+  const isImage = e.target.classList.contains("gallery__image");
+
+  if (!isImage) {
     return;
   }
-  createModalWindow(element.target).show();
+  createModal(e.target).show();
 }
 
-function createModalWindow(params) {
+function createModal(params) {
   const html = `<div class="modal">
   <img src="${params.dataset.source}" alt="${params.alt}" />
- </div>`;
+  </div>`;
 
-  modalWindow = basicLightbox.create(html, {
-    modalShow: () => {
-      window.addEventListener("keyup", closeModalKeyUp);
+  modal = basicLightbox.create(html, {
+    onShow: () => {
+      window.addEventListener("keyup", onCloseModalKeyUp);
     },
-    modalClose: () => {
-      window.removeEventListener("keyup", closeModalKeyUp);
+    onClose: () => {
+      window.removeEventListener("keyup", onCloseModalKeyUp);
     },
   });
-  return modalWindow;
+  return modal;
 }
 
-function closeModalKeyUp(element) {
-  console.log(element.code);
-  if (element.code !== "Escape") return;
+function onCloseModalKeyUp(e) {
+  console.log(e.code);
+  if (e.code !== "Escape") return;
   modal.close();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,9 +7,9 @@ const gallery = document.querySelector(".gallery");
 gallery.insertAdjacentHTML("afterbegin", galeryGard);
 
 function createGalleryItem({ preview, original, description }) {
-  return `<a class="gallery__item" href="${original}">
+  return `<li><a class="gallery__item" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
-  </a>`;
+  </a></li>`;
 }
 
 let lightbox = new SimpleLightbox(".gallery a", {
